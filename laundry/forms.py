f9fb7ac4cf7553018057d3a856541db4153fc69c
klu4156 <klu@imsa.edu>
2017-07-05 22:09:31 -0500

    Login actually works now

--- laundry/forms.py
+++ laundry/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from .models import Queue_Entry
 
 class UserForm(forms.ModelForm):
 	class Meta:
@@ -10,3 +11,8 @@ class UserLogin(forms.ModelForm):
 	class Meta:
 		model = User
 		fields = ('username', 'password')
+
+class QueueForm(forms.ModelForm):
+	class Meta:
+		model = Queue_Entry
+		fields = ('machine',)
--- laundry/views.py
+++ laundry/views.py
@@ -2,18 +2,30 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Machine, Queue_Entry
 from django.contrib.auth.decorators import login_required
-from .forms import UserForm
+from .forms import UserForm, QueueForm, UserLogin
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
 def home_page(request):
-    return render(request, 'laundry/home_page.html')
+	return render(request, 'laundry/home_page.html')
 
 def hall_page(request, hall):
-    hall_machine = Machine.objects.filter(hall__exact = hall)
-    return render(request, 'laundry/hall_page.html', {'halls': hall_machine})
+	hall_machine = Machine.objects.filter(hall__exact = hall)
+	queue = Queue_Entry.objects
+	'''if request.method == "POST":
+		form = QueueForm(request.POST)
+		if form.is_valid():
+			#current_user = request.POST['user']
+			new_entry = Queue_Entry.objects.create
+			machine = request.POST['machine']
+			machine.startLaundry()
+			return redirect('hall_page')
+	else:
+		form = QueueForm()'''
+	return render(request, 'laundry/hall_page.html', {'halls': hall_machine}, {'queue': queue}, {'form':form})
+	
 
 def user_new(request):
 	if request.method == "POST":
@@ -27,11 +39,14 @@ def user_new(request):
 	return render(request, 'laundry/user_edit.html', {'form':form})
 
 def user_login(request):
-	username = request.POST['username']
-	password = request.POST['password']
-	user = authenticate(request, username=username, password=password)
-	if user is not None:
-		login(request,user)
-		return redirect('home_page')
+	if request.method == "POST":
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request,user)
+			return redirect('home_page')
+		#ADD REDIRECT TO USER NOT FOUND PAGE
 	else:
-		return #error message, invalid login
+		form = UserLogin()
+	return render(request, 'laundry/user_login.html', {'form':form})
